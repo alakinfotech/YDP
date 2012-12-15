d0443e19cae9b41091e06f29cbe353a3fbd9b64b
Revanth <rtondapu@alakinfotech.com>
2012-12-16 01:52:11 +0530

    Added POC on QRCode for Android.

---
+++ Android/POC/ZBarAndroidSDK-0.2/examples/CameraTest/gen/net/sourceforge/zbar/android/CameraTest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.sourceforge.zbar.android.CameraTest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
