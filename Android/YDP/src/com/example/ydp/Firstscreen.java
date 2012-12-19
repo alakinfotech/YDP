735aeefbc2f65ab6fc121f61faebe099259c51dc
srikanth <gajula.srikanth4@gmail.com>
2012-12-19 10:52:12 -0500

    Implement two tabs ,user interface_1 and UI_2.UI_1 consist of user login .UI_2 consist of options for users.

---
+++ Android/YDP/src/com/example/ydp/Firstscreen.java
@@ -0,0 +1,15 @@
+package com.example.ydp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Firstscreen extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ydpcareplan);
+		
+	}
+
+}
