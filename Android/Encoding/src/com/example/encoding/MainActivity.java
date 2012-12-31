b6022a14f5e7ae4b9853b36e95f7f3328a6fd52f
RevanthReddyT <reddy.revanth@gmail.com>
2012-12-31 21:36:34 +0530

    Fixed problem in POC

--- Android/Encoding/src/com/example/encoding/MainActivity.java
+++ Android/Encoding/src/com/example/encoding/MainActivity.java
@@ -22,24 +22,29 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.ShortBufferException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
  
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchProviderException;
 import java.security.spec.AlgorithmParameterSpec;
 import java.security.SecureRandom;
-
+//http://www.java2s.com/Code/Java/Security/EncryptionanddecryptionwithAESECBPKCS7Padding.htm
  
 public class MainActivity extends Activity {
 	
  
- private String key ="srikanth";
+
 EditText inputTxtFld,encryptTxtFld,decryptTxtFld;
 Button encryptbtn,decryptbtn;
  public	String inputStr,encryptedStr,decryptedStr;
- public byte[] encryptedData,decryptedData,secretKeyData;
+ public byte[] cipherText;
+ public int ctLength;
+SecretKeySpec key;
+byte[] keyBytes;
 
  @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +54,14 @@ Button encryptbtn,decryptbtn;
     	inputTxtFld = (EditText) findViewById(R.id.editText1);
     	encryptTxtFld = (EditText) findViewById(R.id.editText2);
     	decryptTxtFld = (EditText) findViewById(R.id.editText3);
+
     	
-    	
-		    	
+		keyBytes = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+		        0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
+
+		
+		
+		key = new SecretKeySpec(keyBytes, "AES");
     	
     	
     	encryptbtn = (Button) findViewById(R.id.button1);
@@ -67,32 +77,23 @@ Button encryptbtn,decryptbtn;
 		    	
 				
 				try {
-				
-					KeyGenerator kgen = KeyGenerator.getInstance("AES");
-					SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-					sr.setSeed(key.getBytes());
-					kgen.init(128, sr); // 192 and 256 bits may not be available
-					SecretKey skey = kgen.generateKey();
-					secretKeyData = skey.getEncoded();
-					
-					
-					  
-					  SecretKeySpec encodeSpec = new SecretKeySpec(secretKeyData, "AES");
-					  Cipher encipher = Cipher.getInstance("AES");
-					  encipher.init(Cipher.ENCRYPT_MODE, encodeSpec);
-					  encryptedData = encipher.doFinal(inputStr.getBytes());
-					  encryptedStr = new String(encryptedData.toString());
-					 // Toast.makeText(getApplicationContext(),encryptedStr, 3000).show();
-					
-					  encryptTxtFld.setText(encryptedStr);
+					byte[] input = inputStr.getBytes();
+					//Toast.makeText(MainActivity.this, new String("Adi&Revanth".getBytes()), Toast.LENGTH_SHORT).show();
+					Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+				    // encryption pass
+				    cipher.init(Cipher.ENCRYPT_MODE, key);
+
+				    cipherText = new byte[cipher.getOutputSize(input.length)];
+				    ctLength = cipher.update(input, 0, input.length, cipherText, 0);
+				    ctLength += cipher.doFinal(cipherText, ctLength);
+				    encryptTxtFld.setText(new String(cipherText));
+				    System.out.println(new String(cipherText));
+				    System.out.println(ctLength);
+
+				    
+					 
 
 					
-				} catch (NoSuchAlgorithmException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (NoSuchPaddingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				} catch (InvalidKeyException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -125,20 +126,18 @@ decryptedStr = encryptTxtFld.getText().toString();
 		    	
 				
 				try {
-
-					SecretKeySpec decodeSpec = new SecretKeySpec(secretKeyData, "AES");
-					Cipher cip = Cipher.getInstance("AES");
-					  cip.init(Cipher.DECRYPT_MODE, decodeSpec);
-					  decryptedData = cip.doFinal(encryptedData);
-					  decryptedStr = new String(decryptedData.toString());
-					  //Toast.makeText(getApplicationContext(),decryptedStr, 3000).show();
-					  decryptTxtFld.setText(decryptedStr);
-				} catch (NoSuchAlgorithmException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (NoSuchPaddingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+					
+					// decryption pass
+				    cipher.init(Cipher.DECRYPT_MODE, key);
+					byte[] plainText = new byte[cipher.getOutputSize(ctLength)];
+					int ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
+				    ptLength += cipher.doFinal(plainText, ptLength);
+				    decryptTxtFld.setText(new String(plainText));
+				    System.out.println(new String(plainText));
+				    System.out.println(ptLength);
+				    Toast.makeText(MainActivity.this, new String(plainText), Toast.LENGTH_SHORT).show();
+				    
 				} catch (InvalidKeyException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -146,6 +145,18 @@ decryptedStr = encryptTxtFld.getText().toString();
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} catch (BadPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ShortBufferException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchProviderException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchPaddingException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
