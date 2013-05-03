a7ab0c0def95770c2263dd2d804b921f5c906188
Srikanth <gajula.srikanth4@gmail.com>
2013-05-03 17:34:14 +0530

    Commit-2: YDPCardDataEncoder UserInterface design without implementation

--- Desktop Applications/YDPCardDataEncoder/src/com/alakinfotech/ydpcardencoder/YDPCardEncoder.java
+++ Desktop Applications/YDPCardDataEncoder/src/com/alakinfotech/ydpcardencoder/YDPCardEncoder.java
@@ -12,6 +12,10 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 /** copy rights to Alakinfotech
  * Date:03/05/2013 
@@ -28,6 +32,8 @@ public class YDPCardEncoder extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField inputtxtfld;
+	private JTextField outputtxtfld;
 
 
 	/**
@@ -70,8 +76,51 @@ public class YDPCardEncoder extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel titlelbl = new JLabel("Your Doctor Program");
+		titlelbl.setForeground(new Color(51, 102, 153));
+		titlelbl.setHorizontalAlignment(SwingConstants.CENTER);
+		titlelbl.setIcon(new ImageIcon("D:\\AIT_PROJECTS\\YDP_MOBILE\\Desktop Applications\\YDPCardDataEncoder\\app_icon.png"));
+		titlelbl.setBackground(new Color(51, 102, 153));
+		titlelbl.setBounds(12, 13, 652, 38);
+		contentPane.add(titlelbl);
+		
+		JLabel inputtxtlbl = new JLabel("WorkBook Input File:");
+		inputtxtlbl.setBounds(23, 98, 129, 16);
+		contentPane.add(inputtxtlbl);
+		
+		inputtxtfld = new JTextField();
+		inputtxtfld.setBounds(166, 95, 360, 22);
+		contentPane.add(inputtxtfld);
+		inputtxtfld.setColumns(10);
+		
+		JButton inputbtn = new JButton("Browse..");
+		inputbtn.setForeground(Color.WHITE);
+		inputbtn.setBackground(new Color(51, 102, 153));
+		inputbtn.setBounds(538, 94, 97, 25);
+		contentPane.add(inputbtn);
+		
+		JLabel outputlbl = new JLabel("WorkBook Output File:");
+		outputlbl.setBounds(23, 186, 129, 16);
+		contentPane.add(outputlbl);
+		
+		outputtxtfld = new JTextField();
+		outputtxtfld.setBounds(168, 183, 360, 22);
+		contentPane.add(outputtxtfld);
+		outputtxtfld.setColumns(10);
+		
+		JButton outputbtn = new JButton("Browse..");
+		outputbtn.setForeground(Color.WHITE);
+		outputbtn.setBackground(new Color(51, 102, 153));
+		outputbtn.setBounds(538, 182, 97, 25);
+		contentPane.add(outputbtn);
+		
+		JButton encodebtn = new JButton("Start Encoding");
+		encodebtn.setForeground(Color.WHITE);
+		encodebtn.setBackground(new Color(51, 102, 153));
+		encodebtn.setBounds(258, 285, 186, 43);
+		contentPane.add(encodebtn);
 	}
-
 }
